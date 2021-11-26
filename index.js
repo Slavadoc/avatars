9f452ff6a544d88b0b8b8907204d695e7c12dab5
Slavadoc <viaceslavziablov@gmail.com>
2021-11-26 17:13:18 +0300

    17 10 26.11

---
+++ index.js
@@ -0,0 +1,18 @@
+/*let button = document.querySelector('btn'); - doesn't work 
+
+console.log(button);
+/*button.onclick = () =>  {
+   console.log("it's work");
+   alert("Javascript подключен");
+}
+
+/*const btn = document.querySelector('button');
+
+function random(number) {
+  return Math.floor(Math.random() * (number+1));
+}
+
+btn.onclick = function() {
+  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = rndCol;
+}*/
