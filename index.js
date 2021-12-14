39a33ea43563290e0f7569384a0489525b8b4294
Slavadoc <viaceslavziablov@gmail.com>
2021-12-14 19:01:22 +0300

    smile

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
+// let getCtc = () => {
+//   let ct = canvas.getContext('2d')
+// }
+
  let aCircle = () => {
+   // getCtc()
    let ct = canvas.getContext('2d')
    ct.beginPath()
    ct.arc(150, 150, 100, 0, 2 * Math.PI)
@@ -11,21 +16,21 @@
  let aTriangle = () => {
    let ctc = canvas.getContext('2d')
    ctc.beginPath()
-   ctc.lineTo(150, 150)
-   ctc.lineTo(200, 200)
+   ctc.lineTo(150, 100)
+   ctc.lineTo(200, 150)
    ctc.lineTo(100, 200)
-   ctc.lineTo(150, 10)
+   ctc.lineTo(150, 100)
    ctc.closePath()
    ctc.strokeStyle = '#6949D7'
-   ctc.fillStyle = '#6949D7	'
+   ctc.fillStyle = '#6949D7'
    ctc.fill()
    ctc.stroke()
  }
  let aSmile = () => {
-   alert('worked')
-   let cts = canvas.getContext('2b')
-   cts.lineWidth = 5; // толщина линии
-   cts.strokeStyle = '#151619'
-   cts.arc(75,75,35,0,Math.PI,false);
-   ctx.stroke();
+   let cts = canvas.getContext('2d')
+   cts.beginPath()
+   cts.arc(150,175,50 ,0 , Math.PI)
+   cts.lineWidth = 5 // толщина линии
+   cts.strokeStyle = '#6949D7'
+   ctx.stroke()
  }
